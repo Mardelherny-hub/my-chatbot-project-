fa8f4b10ba0ebbcbf9a7d52a8398668a65bf0fce
Victor Alcalde <alcaldevictor1@gmail.com>
2024-07-23 22:53:42 -0300

    Commit inicial con Rasa

---
+++ actions/actions.py
@@ -0,0 +1,47 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from langdetect import detect
+
+class ActionDetectLanguage(Action):
+    def name(self) -> Text:
+        return "action_detect_language"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        user_message = tracker.latest_message.get('text')
+        try:
+            detected_language = detect(user_message)
+        except:
+            detected_language = "en"  # default to English if detection fails
+        
+        return [{"name": "language", "value": detected_language}]
